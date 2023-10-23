42e37beb0528a1a2e69056f9d245d4dbbd906a7b
subframe7536 <1667077010@qq.com>
2023-10-23 10:07:47 +0800

    feat: update terminal color

--- src/modules/ui/terminal.ts
+++ src/modules/ui/terminal.ts
@@ -1,38 +1,54 @@
-import type { GenerateUIWithBaseFn } from '../../type'
-import { darken } from '../../util'
+import type { GenerateUIWithBaseFn, UI } from '../../type'
 
 const black = '#181818'
 const white = '#f4f4f5'
 export const generateTerminalColor: GenerateUIWithBaseFn = (baseColor, ui, isDark) => {
-  if (!isDark) {
-    for (const [key, color] of Object.entries(baseColor)) {
-      baseColor[key] = darken(color, 8)
-    }
-  }
+  const terminal: UI['terminal'] = isDark
+    ? {
+        background: ui.backgroundEditor,
+        foreground: ui.foreground,
+        ansiBrightBlack: black,
+        ansiBrightBlue: baseColor.blue,
+        ansiBrightCyan: baseColor.cyan,
+        ansiBrightGreen: baseColor.green,
+        ansiBrightMagenta: baseColor.purple,
+        ansiBrightRed: baseColor.red,
+        ansiBrightWhite: white,
+        ansiBrightYellow: baseColor.yellow,
+        ansiBlack: black,
+        ansiBlue: baseColor.blue,
+        ansiCyan: baseColor.cyan,
+        ansiGreen: baseColor.green,
+        ansiMagenta: baseColor.purple,
+        ansiRed: baseColor.red,
+        ansiWhite: white,
+        ansiYellow: baseColor.yellow,
+      }
+    : {
+        foreground: '#393a34',
+        selectionBackground: '#22222215',
+        ansiBrightBlack: '#aaaaaa',
+        ansiBrightBlue: '#296aa3',
+        ansiBrightCyan: '#2993a3',
+        ansiBrightGreen: '#1e754f',
+        ansiBrightMagenta: '#a13865',
+        ansiBrightRed: '#ab5959',
+        ansiBrightWhite: '#dddddd',
+        ansiBrightYellow: '#bda437',
+        ansiBlack: '#121212',
+        ansiBlue: '#296aa3',
+        ansiCyan: '#2993a3',
+        ansiGreen: '#1e754f',
+        ansiMagenta: '#a13865',
+        ansiRed: '#ab5959',
+        ansiWhite: '#dbd7caee',
+        ansiYellow: '#bda437',
+      }
   return {
     terminalCursor: {
       background: ui.backgroundEditor,
       foreground: ui.foreground,
     },
-    terminal: {
-      background: ui.backgroundEditor,
-      foreground: ui.foreground,
-      ansiBrightBlack: black,
-      ansiBrightBlue: baseColor.blue,
-      ansiBrightCyan: baseColor.cyan,
-      ansiBrightGreen: baseColor.green,
-      ansiBrightMagenta: baseColor.purple,
-      ansiBrightRed: baseColor.red,
-      ansiBrightWhite: white,
-      ansiBrightYellow: baseColor.yellow,
-      ansiBlack: black,
-      ansiBlue: baseColor.blue,
-      ansiCyan: baseColor.cyan,
-      ansiGreen: baseColor.green,
-      ansiMagenta: baseColor.purple,
-      ansiRed: baseColor.red,
-      ansiWhite: white,
-      ansiYellow: baseColor.yellow,
-    },
+    terminal,
   }
 }
