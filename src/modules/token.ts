d80884bd909d47b80ffc837a9c99632102393806
subframe7536 <1667077010@qq.com>
2024-12-19 15:21:51 +0800

    feat: italic comment

--- src/modules/token.ts
+++ src/modules/token.ts
@@ -36,10 +36,17 @@ export function generateTokenColor(tokens: TokenColor, plainColor: string) {
     {
       scope: [
         'comment',
-        'punctuation.definition.comment',
         'string.comment',
       ],
       foreground: maple('comment'),
+      fontStyle: font('italic'),
+    },
+    {
+      scope: [
+        'punctuation.definition.comment',
+      ],
+      foreground: maple('comment'),
+      fontStyle: font(),
     },
     {
       scope: [
